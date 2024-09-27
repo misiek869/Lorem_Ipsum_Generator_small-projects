906219e5d8144be0f2db324e2c6b1135028e81e0
Michal Gawik <gawlik.zator@o2.pl>
2024-09-27 11:52:32 +0200

    set first logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,39 @@
 import { useState } from 'react'
+import data from './data'
 
 function App() {
-	const [count, setCount] = useState(0)
+	const [count, setCount] = useState<number>(1)
+	const [text, setText] = useState<string[]>(data)
 
-	return <div></div>
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setCount(Number(e.target.value))
+	}
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+
+		const newArray = data.slice(0, count)
+		setText(newArray)
+	}
+
+	return (
+		<main>
+			<form action='' onSubmit={handleSubmit}>
+				<label htmlFor='number'>Select Number</label>
+				<input
+					type='number'
+					name='number'
+					id='number'
+					value={count}
+					min={1}
+					max={8}
+					step={1}
+					onChange={handleChange}
+				/>
+			</form>
+			<p>{text}</p>
+		</main>
+	)
 }
 
 export default App
